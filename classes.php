c9f12cbfc5309081bff0ec605187fa12068c9714
Lukasz Gogolin <lukasz.gogolin@xcaliber.com>
2017-02-08 12:42:52 +0100

    Initial commit

---
+++ classes.php
@@ -0,0 +1,11 @@
+<?php
+
+// 
+// NOTE!
+// 
+// This is an auto generated file. Changes to this file will be overwritten!
+// 
+return array (
+  'grafana' => 'helpers/grafana.php',
+  'grafana_controller' => 'controllers/grafana.php',
+);
